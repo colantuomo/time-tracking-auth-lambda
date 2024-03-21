cfefa68c260683f1e05c525d7df4cd02faeeda64
colantuomo <pcolantuomo@gmail.com>
2024-03-20 21:24:29 -0300

    feat: adding login and register route

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
 import { Handler } from "aws-lambda";
-import { register, login } from './src'
 export const handler: Handler = async (event, context) => {
-    return `${register()} - ${login()} [${event.body.toString()}]`;
+    const requestBody = JSON.parse(event.body);
+    const { login, register } = event.pathParameters;
+    const { matricula, password } = requestBody;
+    if (login) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(`register route ${matricula}`)
+        };
+    }
+    if (register) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(`register route ${matricula}`)
+        };
+    }
+    return {
+        statusCode: 404,
+        body: JSON.stringify(`route not found`)
+    };
 };
