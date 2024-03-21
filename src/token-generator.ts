d337f2a3dc0f3bdc716b82ee9931667ef44186ec
colantuomo <pcolantuomo@gmail.com>
2024-03-20 23:56:54 -0300

    chore: using jose as jwt lib

--- src/token-generator.ts
+++ src/token-generator.ts
@@ -1,5 +1,12 @@
-import jwt from "jsonwebtoken";
+import * as jose from 'jose'
 
-export function generateToken(user: string, password: string) {
-    return jwt.sign({ user, password }, process.env.SECRET_KEY as string, { expiresIn: "24h" });
+export async function generateToken(user: string, password: string) {
+    const secret = new TextEncoder().encode(process.env.SECRET_KEY)
+    const alg = 'HS256'
+
+    return new jose.SignJWT({ user, password })
+        .setProtectedHeader({ alg, typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('24h')
+        .sign(secret)
 };
