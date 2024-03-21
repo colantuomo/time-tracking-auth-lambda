949f8587336bce5bfceb95e42f3b220afdf73a1f
colantuomo <pcolantuomo@gmail.com>
2024-03-20 21:17:32 -0300

    ci: see if parameter is show

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import { Handler } from "aws-lambda";
 import { register, login } from './src'
 export const handler: Handler = async (event, context) => {
-    const requestBody = JSON.parse(event.body);
-    // Extracting parameters from the parsed JSON
-    // const { matricula, password } = requestBody;
-
     return `${register()} - ${login()} [${event.body.toString()}]`;
 };
